e592bef1e1af43362b1bf8f0107435f232f96839
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-06-20 21:51:59 -0500

    basic layout and template

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 console.log('hi');
 
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+      var $a = $(".navbar-default .nav > li > a")
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $a.toggleClass('scrolled', $(this).scrollTop() > $a.height());
+  });
+});
 
- 
+
+ $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top-50
+    }, 500);
+    return false;
+});
